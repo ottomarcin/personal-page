9adda5c4bcb4071a04a0b5be4430e83481489635
Marcin Otto <ottomarcin96@gmail.com>
2022-10-25 14:47:16 +0200

    Minor changes

--- src/components/Experience/Loader.jsx
+++
@@ -1,9 +0,0 @@
-import { useProgress, Html } from '@react-three/drei';
-import React from 'react';
-
-function Loader(props) {
-  const { active, progress, errors, item, loaded, total } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
-}
-
-export default Loader;
